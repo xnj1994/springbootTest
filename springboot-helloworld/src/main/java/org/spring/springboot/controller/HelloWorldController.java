665914c37b062cc30da60410c4e3cfded771ec51
xnj1994 <1461477734@qq.com>
2018-07-16 23:32:15 +0800

    commit

--- springboot-helloworld/src/main/java/org/spring/springboot/controller/HelloWorldController.java
+++ springboot-helloworld/src/main/java/org/spring/springboot/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package org.spring.springboot.web;
+package org.spring.springboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
